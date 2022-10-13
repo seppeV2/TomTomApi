e00351d5b6cb0686e8c9774fd00fe939108cc4be
seppeV2 <seppe.vilain@student.kuleuven.be>
2022-10-13 11:47:42 +0200

    Minor Changes
    small changes still WIP

--- ODRequest.py
+++ ODRequest.py
@@ -4,7 +4,7 @@ import json
 API_Key1 = "m48ctXO8JwRAMDkRxbq2yqtngG3dsO7F"
 API_Key2 = "Gcw5ye6iGdXCPqtOllT2idD7OUmOUIG7"
 API_Key3 = "ealGbxH7zbd5RKJg94Yz4yvnrB2d0cu1"
-url = "https://api.tomtom.com/origindestination/1/analysis/flowmatrix?key={"+API_Key1+"}"
+url = "https://api.tomtom.com/origindestination/1/analysis/flowmatrix?key="+API_Key1
 
 
 body = open('body.JSON')
